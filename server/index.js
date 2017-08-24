6458adb309e36a8e629223218ce172908cdfb995
Artem Rosnovskiy <artem@rosnovsky.us>
2017-08-24 14:09:15 -0700

    🔥 Authentication workflow complete (Google OAuthe 2.0) 🔥

--- server/index.js
+++ server/index.js
@@ -1,9 +1,26 @@
-const express = require('express');
-require('./services/passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 
+require("./models/user");
+require("./services/passport");
+const keys = require("./config/keys");
+
+mongoose.connect(keys.mongoURI);
 const app = express();
 
-require('./routes/authRoutes')(app);
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
 
-const PORT = process.env.PORT || 5000; 
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,13 +1,40 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../config/keys');
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
 
-passport.use(new GoogleStrategy(
+const Users = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    Users.findById(id)
+    .then(user => {
+        done(null, user);
+    });
+});
+
+
+
+passport.use(
+  new GoogleStrategy(
     {
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback' 
-    }, 
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
     (accessToken, refreshToken, profile, done) => {
-    console.log(`Logged in as ${profile.displayName} (${profile.emails[0].value})`);
-}));
+      Users.findOne({ googleID: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new Users({ googleID: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
+    }
+  )
+);
