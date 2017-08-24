3e4f14f22b27a26ea2541ade5c0ee3fdfe3453a9
Artem Rosnovskiy <artem@rosnovsky.us>
2017-08-23 21:47:55 -0700

    Basic authentication complete 🔐D

--- server/index.js
+++ server/index.js
@@ -1,9 +1,31 @@
-const express = require("express");
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there' });
-});
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback' 
+}, (profile, done) => {
+    console.log('Name', profile.name.givenName);
+}));
+
+app.get(
+    '/auth/google', 
+    passport.authenticate("google", {
+    scope: ['profile', 'email']
+    })
+);
+
+app.get(
+    '/auth/google/callback',
+    passport.authenticate('google') 
+);
+
+
 
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT);
