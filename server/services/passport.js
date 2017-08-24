dc805dbac13ab7f94727220a0be30cbf6c323b5b
Artem Rosnovskiy <artem@rosnovsky.us>
2017-08-24 15:42:24 -0700

    UUpdated Heroku settings

--- server/services/passport.js
+++ server/services/passport.js
@@ -23,7 +23,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       Users.findOne({ googleID: profile.id }).then(existingUser => {
