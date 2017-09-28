bc7b1bfc6d836a8157b43e36dac00e52868896e6
Artem Rosnovskiy <artem@rosnovsky.us>
2017-09-27 20:15:01 -0700

    Heroku deployment groundwork

---
+++ server/README.md
@@ -0,0 +1,5 @@
+# Send out Surveys, fast
+
+![Latest Build](https://travis-ci.org/rosnovsky/emaily-app.svg?branch=master)
+
+_Expand description_
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
-const bodyBarser = require('body-parser');
+const bodyBarser = require("body-parser");
 const passport = require("passport");
 
 require("./models/user");
@@ -11,8 +11,6 @@ const keys = require("./config/keys");
 mongoose.connect(keys.mongoURI);
 const app = express();
 
-
-
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -20,14 +18,20 @@ app.use(
   })
 );
 
-
 app.use(bodyBarser.json());
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
+
+if (process.env.NODE_MODE === "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
