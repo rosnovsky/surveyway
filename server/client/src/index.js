3aa1e98286d983981fa0f0b8eb255979b3472b5d
Artem Rosnovskiy <artem@rosnovsky.us>
2017-09-27 22:02:14 -0700

    disable node_modules cache

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,7 +1,4 @@
-// data -> redux
-
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
