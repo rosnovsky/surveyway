c861ce5799805cc5cd77879c7d39355153f778af
Artem Rosnovskiy <artem@rosnovsky.us>
2017-09-28 08:20:39 -0700

    fixed a typo in stripe workfloy

--- server/config/prod.js
+++ server/config/prod.js
@@ -4,5 +4,5 @@ module.exports = {
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY,
     stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-    stripeSecretleKey: process.env.STRIPE_SECRET_KEY,
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY,
 };
