7ba39296a6769d0c845bd9589eec2a08d70dd908
Artem Rosnovskiy <artem@rosnovsky.us>
2018-03-07 11:47:10 -0800

    picking up where left off

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,6 +1,14 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
+// Take a look at this for color scheme: https://coolors.co/
+
+import React, {
+  Component
+} from "react";
+import {
+  Link
+} from "react-router-dom";
+import {
+  connect
+} from "react-redux";
 
 class Landing extends Component {
   renderContent() {
@@ -8,35 +16,49 @@ class Landing extends Component {
       case null:
         return;
       case false:
-        return (
-          <p>
-            No registration, just <a href="/auth/google">login with Google</a>{" "}
-            to proceed.
-          </p>
+        return ( <
+          p >
+          No registration, just < a href = "/auth/google" > login with Google < /a>{" "}
+          to proceed. <
+          /p>
         );
       default:
-        return (
-          <p>
-            What are you waiting for?{" "}
-            <Link to={"/surveys/new"}>Create a survey</Link> or add credits to
-            your account.
-          </p>
+        return ( <
+          p >
+          What are you waiting
+          for ? {
+            " "
+          } <
+          Link to = {
+            "/surveys/new"
+          } > Create a survey < /Link> or add credits to
+          your account. <
+          /p>
         );
     }
   }
 
   render() {
-    return (
-      <div style={{ textAlign: "center" }}>
-        <h1>The Survey Way</h1>
-        <h5>Send out surveys, fast</h5>
-        <div>{this.renderContent()}</div>
-      </div>
+    return ( <
+      div style = {
+        {
+          textAlign: "center"
+        }
+      } >
+      <
+      h1 > The Survey Way < /h1> <
+      h5 > Send out surveys, fast < /h5> <
+      div > {
+        this.renderContent()
+      } < /div> < /
+      div >
     );
   }
 }
 
-function mapStateToProps({ auth }) {
+function mapStateToProps({
+  auth
+}) {
   return {
     auth
   };
