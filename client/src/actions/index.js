044ca3e353585e7f5b9e8503662f954feec82312
Artem Rosnovskiy <artem@rosnovsky.us>
2018-03-11 20:53:22 -0700

    🎉 Application finalized 🎉

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { FETCH_USER } from "./types";
+import { FETCH_USER, FETCH_SURVEYS } from "./types";
+
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get("/api/current_user");
@@ -26,6 +27,15 @@ export const submitSurvey = (values, history) => async dispatch => {
 
   dispatch({
     type: FETCH_USER,
+    payload: res.data
+  });
+};
+
+export const fetchSurveys = () => async dispatch => {
+  const res = await axios.get("/api/surveys");
+
+  dispatch({
+    type: FETCH_SURVEYS,
     payload: res.data
   });
 };
