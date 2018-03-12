b9304f278399fec41982a1cf283b3edb1517c9a9
Artem Rosnovskiy <artem@rosnovsky.us>
2018-03-11 19:04:05 -0700

    Feedback all wired up

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 ![Latest Build](https://travis-ci.org/rosnovsky/surveyway.svg?branch=master)
 
-_Expand description_
+## The Survey Way
+
+This app uses Node, Express, MongoDB (via mLab), React and Redux at its core.
+
+Passport.js provides user authentication via Google oAuth API
+
+Sendgrid is email services provider.
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require("lodash");
+const Path = require("path-parser");
+const { URL } = require("url");
 const mongoose = require("mongoose");
 
 const requireLogin = require("../middleware/requireLogin");
@@ -5,15 +8,41 @@ const requireCredits = require("../middleware/requireCredits");
 
 const Survey = mongoose.model("survey");
 const Mailer = require("../services/Mailer");
-const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
+const surveyTemplate = require("../services/emailTemplates/surveyTemplate");  
 
 module.exports = app => {
-  app.get("/api/surveys/thanks", (req, res) => {
+  app.get("/api/surveys/:surveyId/:choice", (req, res) => {
     res.send("Thanks for voting!");
   });
 
   app.post("/api/surveys/webhooks", (req, res) => {
-    console.log(req.body);
+    const p = new Path("/api/surveys/:surveyId/:choice");    
+
+    console.log(_.chain(req.body)
+    .map(({ email, url }) => {
+      const match = p.test(new URL(url).pathname);
+      if(match){
+        return { email, surveyId: match.surveyId, choice: match.choice };
+      }
+    })
+    .compact()
+    .uniqBy('email', 'surveyId')
+    .each(({ surveyId, email, choice }) => {
+      Survey.updateOne(
+        {
+          _id: surveyId,
+          recipients: {
+            $elemMatch: { email: email, responded: false }
+          }
+        },
+        {
+          $inc: { [choice]: 1 },
+          $set: { 'recipients.$.responded' : true },
+          lastResponded: new Date()
+        }).exec();
+      })
+    .value());
+
     res.send({});
   });
 
@@ -36,10 +65,8 @@ module.exports = app => {
 
       req.user.credits -= 1;
       const user = await req.user.save();
-      console.log(user);
       res.send(user);
     } catch (err) {
-      console.log(`Err: ${err}`);
       res.status(422).send(err);
     }
   });
